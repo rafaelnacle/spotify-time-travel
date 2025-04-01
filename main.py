33e258dcc228cecdf84fe4d1795fecb800b575b0
= <=>
2025-04-01 15:53:11 -0300

    search spotify for the songs from billboard

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ SPOTIFY_SECRET = os.getenv("SPOTIFY_SECRET")
 USER = os.getenv("USER")
 REDIRECT_URL = "https://open.spotify.com/"
 
-scope = "playlist-modify-private"
-
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
+        scope = "playlist-modify-private",
         client_id=SPOTIFY_ID,
         client_secret=SPOTIFY_SECRET,
         redirect_uri=REDIRECT_URL,
@@ -25,17 +24,29 @@ sp = spotipy.Spotify(
     )
 )
 
-user_id = sp.current_user()["id"]
-
-print(user_id)
-
 date = input("What year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 url = "https://www.billboard.com/charts/hot-100/" + date
 response = requests.get(url=url, headers=header)
 
 soup = BeautifulSoup(response.text, "html.parser")
 song_names_spans = soup.select("li ul li h3")
-print(song_names_spans)
 song_names = [song.getText().strip() for song in song_names_spans]
+
+user_id = sp.current_user()["id"]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
+
+
+
